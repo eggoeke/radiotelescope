e21c5bf163fd13dd71e89a9ac340efd4e4e15827
eggoeke <goekeee@guilford.edu>
2017-01-06 11:46:10 -0500

    adding html to py script

--- app/templates/renderCalendar.py
+++ app/templates/renderCalendar.py
@@ -1,13 +1,17 @@
 import calendar
 import sys
-import posixpath
-import os
 
 def renderCal(month, year, equip):
 	calendar.setfirstweekday(calendar.SUNDAY)
 	mat = calendar.monthcalendar(year, month)
 	render = open("renderCal.html", 'w')
-	render.write(str(mat.__len__()))
+	str1 = ('<table border="1" cellpadding="0" cellspacing="0" class="month">'
+	'<tr><th colspan="7" class="month">' + calendar.month_name[month] +' '+ str(year) + '</th></tr>'
+	'<tr><th class="sun">Sun</th><th class="mon">Mon</th><th class="tue">Tue</th><th class="wed">Wed</th>'
+	'<th class="thu">Thu</th><th class="fri">Fri</th><th class="sat">Sat</th></tr>'
+	)
+
+	render.write(str1)
 	render.close()
 
 	return
