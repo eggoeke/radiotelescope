1debca06992b7d58c4e13c84e9219c408668cc1c
eggoeke <goekeee@guilford.edu>
2017-01-06 11:11:40 -0500

    writing local html for calendar via python (:

--- app/templates/renderCalendar.py
+++ app/templates/renderCalendar.py
@@ -1,6 +1,13 @@
 import calendar
+import sys
+import posixpath
+import os
 
-def renderCal(month, year, equip)
-    calendar.setfirstweekday(calendar.SUNDAY)
-    calendar.monthcalendar(year, month)
-    return
+def renderCal(month, year, equip):
+	calendar.setfirstweekday(calendar.SUNDAY)
+	mat = calendar.monthcalendar(year, month)
+	render = open("renderCal.html", 'w')
+	render.write(str(mat.__len__()))
+	render.close()
+
+	return
