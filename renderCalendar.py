ed888ab68d933c17b0bc40cb35c477e6447039db
eggoeke <goekeee@guilford.edu>
2017-01-09 15:57:39 -0500

    calendar can go back/forward infinitely (as calendar module says)

--- renderCalendar.py
+++ renderCalendar.py
@@ -1,8 +1,26 @@
 import calendar
 import sys
 
-def renderCal(month, year, equip):
+
+def renderCal(m, y, equip):
+	global month
+	global year
+	month = m
+	year = y
 	calendar.setfirstweekday(calendar.SUNDAY)
+	temp = 0
+	tempmonth = 1;
+	if(month > 12):
+		global month
+		temp = int(month/12)
+		month = month - 12*temp
+	elif(month < 1):
+		global month
+		tempmonth = month - 12
+		temp = int(tempmonth/12)
+		month = month - 12*temp
+
+	year = year + temp
 	mat = calendar.monthcalendar(year, month)
 	render = open("app/templates/renderCal.html", 'w')
 	str1 = ('{% extends "calendar.html" %}{% block calendar %}<table border="1" cellpadding="0" cellspacing="0" class="month">'
